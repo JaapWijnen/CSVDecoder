b64b2ba9f7be314a5e44203cf011600a72c8d743
Jaap Wijnen <wijnen.jaap@gmail.com>
2019-05-15 11:03:36 +0200

    update package reference to online one

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .library(name: "CSVDecoder", targets: ["CSVDecoder"]),
     ],
     dependencies: [
-        .package(url: "../CSVReader", .branch("master")),
+        .package(url: "git@github.com:JaapWijnen/CSVReader.git", .branch("master")),
     ],
     targets: [
         .target(name: "CSVDecoder", dependencies: ["CSVReader"]),
